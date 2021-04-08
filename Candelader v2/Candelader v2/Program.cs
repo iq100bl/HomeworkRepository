c95995fd5742277dcb6a5b68fcf1bd6cf1df40c8
paul <vsoligorske@gmail.com>
2021-04-08 20:39:20 +0300

    improved the code with the method

--- Candelader v2/Candelader v2/Program.cs
+++ Candelader v2/Candelader v2/Program.cs
@@ -4,6 +4,41 @@ namespace Candelader_v2
 {
     class Program
     {
+
+        static void Daily(DayOfWeek y)
+        {
+            var now = DateTime.Now.DayOfWeek;
+            int weekend = DayOfWeek.Saturday - y;
+
+            if (weekend == 6 || weekend ==0)
+            {
+                
+                if (now == y)
+                {
+                    Console.WriteLine("your choice: {0}, weekday number: {1}, today is the day of your choice and weekend", y, (int)y);
+                }
+                else
+                {
+                    Console.WriteLine("your choice: {0}, weekday number: {1}, it's weekend", y, (int)y);
+                }
+
+            }
+            else
+            {
+                
+                if (now == y)
+                {
+                    Console.WriteLine("your choice: {0}, weekday number: {1}, untill weekend: {2} day(s),today is the day of your choice", y, (int)y, weekend);
+                }
+                else
+                {
+                    Console.WriteLine("your choice: {0}, weekday number: {1}, untill weekend: {2} day(s)", y, (int)y, weekend);
+                }
+
+            }
+
+            Console.ResetColor();
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Write day of the week");
@@ -15,94 +50,37 @@ namespace Candelader_v2
                 case "mon" or "monday":
                     Console.ForegroundColor = ConsoleColor.Red;
                     y = DayOfWeek.Monday;
-                    if (now == Convert.ToInt32(y))
-                    {
-                        Console.WriteLine("your choice: {0}, weekday number: {1}, untill weekend: {2} day(s),today is the day you entered", y, Convert.ToByte(y), 6 - Convert.ToByte(y));
-                    }
-                    else
-                    {
-                        Console.WriteLine("your choice: {0}, weekday number: {1}, untill weekend: {2} day(s)", y, Convert.ToByte(y), 6 - Convert.ToByte(y));
-                    }
-                    Console.ResetColor();
+                    Daily(y);
                     break;
                 case "tue" or "tuesday":
                     Console.ForegroundColor = ConsoleColor.Green;
                     y = DayOfWeek.Tuesday;
-                    if (now == Convert.ToInt32(y))
-                    {
-                        Console.WriteLine("your choice: {0}, weekday number: {1}, untill weekend: {2} day(s),today is the day you entered", y, Convert.ToByte(y), 6 - Convert.ToByte(y));
-                    }
-                    else
-                    {
-                        Console.WriteLine("your choice: {0}, weekday number: {1}, untill weekend: {2} day(s)", y, Convert.ToByte(y), 6 - Convert.ToByte(y));
-                    }
-                    Console.ResetColor();
+                    Daily(y);
                     break;
                 case "wed" or "wednesday":
                     Console.ForegroundColor = ConsoleColor.Yellow;
                     y = DayOfWeek.Wednesday;
-                    if (now == Convert.ToInt32(y))
-                    {
-                        Console.WriteLine("your choice: {0}, weekday number: {1}, untill weekend: {2} day(s),today is the day you entered", y, Convert.ToByte(y), 6 - Convert.ToByte(y));
-                    }
-                    else
-                    {
-                        Console.WriteLine("your choice: {0}, weekday number: {1}, untill weekend: {2} day(s)", y, Convert.ToByte(y), 6 - Convert.ToByte(y));
-                    }
-                    Console.ResetColor();
+                    Daily(y);
                     break;
                 case "thu" or "thursday":
                     Console.ForegroundColor = ConsoleColor.Magenta;
                     y = DayOfWeek.Thursday;
-                    if (now == Convert.ToInt32(y))
-                    {
-                        Console.WriteLine("your choice: {0}, weekday number: {1}, untill weekend: {2} day(s),today is the day you entered", y, Convert.ToByte(y), 6 - Convert.ToByte(y));
-                    }
-                    else
-                    {
-                        Console.WriteLine("your choice: {0}, weekday number: {1}, untill weekend: {2} day(s)", y, Convert.ToByte(y), 6 - Convert.ToByte(y));
-                    }
-                    Console.ResetColor();
+                    Daily(y);
                     break;
                 case "fri" or "friday":
                     Console.ForegroundColor = ConsoleColor.DarkCyan;
                     y = DayOfWeek.Friday;
-                    if (now == Convert.ToInt32(y))
-                    {
-                        Console.WriteLine("your choice: {0}, weekday number: {1}, untill weekend: {2} day(s),today is the day you entered", y, Convert.ToByte(y), 6 - Convert.ToByte(y));
-                    }
-                    else
-                    {
-                        Console.WriteLine("your choice: {0}, weekday number: {1}, untill weekend: {2} day(s)", y, Convert.ToByte(y), 6 - Convert.ToByte(y));
-                    }
-                    Console.ResetColor();
+                    Daily(y);
                     break;
                 case "sat" or "saturday":
                     Console.ForegroundColor = ConsoleColor.Blue;
                     y = DayOfWeek.Saturday;
-                    if (now == Convert.ToInt32(y))
-                    {
-                        Console.WriteLine("your choice: {0}, weekday number: {1}, untill weekend: {2}, today is the day you entered ", y, Convert.ToByte(y), "Weekend");
-                    }
-                    else
-                    {
-                        Console.WriteLine("your choice: {0}, weekday number: {1}, untill weekend: {2} ", y, Convert.ToByte(y), "Weekend");
-                    }
-                    Console.ResetColor();
+                    Daily(y);
                     break;
                 case "sun" or "sunday":
                     Console.ForegroundColor = ConsoleColor.DarkYellow;
                     y = DayOfWeek.Sunday;
-                    y = DayOfWeek.Saturday;
-                    if (now == Convert.ToInt32(y))
-                    {
-                        Console.WriteLine("your choice: {0}, weekday number: {1}, untill weekend: {2}, today is the day you entered ", y, Convert.ToByte(y), "Weekend");
-                    }
-                    else
-                    {
-                        Console.WriteLine("your choice: {0}, weekday number: {1}, untill weekend: {2} ", y, Convert.ToByte(y), "Weekend");
-                    }
-                    Console.ResetColor();
+                    Daily(y);
                     break;
                 default:
                     Console.WriteLine("This is not the day of the week");
