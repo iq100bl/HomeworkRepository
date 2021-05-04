3a8c171bcb5c7a682a6bd88bdb01153e4f863e43
paul <vsoligorske@gmail.com>
2021-05-04 20:12:31 +0300

    add3metods

---
+++ LinqSample-master/LinqSamples/DataAccess/IRepository.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace AnalyticsAdapter
+{
+    public interface IRepository
+    {
+        Order[] GetOrders(int customerId);
+
+        Order GetOrder(int orderId);
+
+        decimal GetMoneySpentBy(int customerId);
+
+        Product[] GetAllProductsPurchased(int customerId);
+
+    //    Product[] GetUniqueProductsPurchased(int customerId);
+
+    //    int GetTotalProductsPurchased(int productId);
+
+    //    bool HasEverPurchasedProduct(int customerId, int productId);
+
+    //    bool AreAllPurchasesHigherThan(int customerId, decimal targetPrice);
+
+    //    bool DidPurchaseAllProducts(int customerId, params int[] productIds);
+
+    //    CustomerOverview GetCustomerOverview(int customerId);
+
+    //    List<(string productName, int numberOfPurchases)> GetProductsPurchased(int customerId);
+    }
+}
---
+++ LinqSample-master/LinqSamples/DataAccess/Repository.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AnalyticsAdapter
+{
+    public class Repository : IRepository
+    {
+        private readonly Database _db;
+
+        public Order[] GetOrders(int customerId)  //получаем список покупок по id пользователя
+        {
+            return _db.Orders.Where(order => order.CustomerId == customerId).ToArray();
+        }
+
+        public Order GetOrder(int orderId) // получаем покупку по её id
+        {
+            var order = _db.Orders.SingleOrDefault(order => order.Id == orderId);
+            if(order == null)
+            {
+                throw new InvalidOperationException("Invalid id order");
+            }
+            return order;
+        }
+        public decimal GetMoneySpentBy(int customerId) //сумма денег, потраченая на покупки одним пользователем
+        {
+            return _db.Orders.Join(
+                _db.Products,
+                a => a.ProductId,
+                b => b.Id,
+                (a, b) => new
+                {
+                    Price = b.Price,
+                    CustomerId = a.CustomerId
+                }).Where(x => x.CustomerId == customerId)
+                .Sum(x => x.Price);
+        }
+        public Product[] GetAllProductsPurchased(int customerId) //все купленые товары пользователя по id 
+        {
+            var productPurchased = GetOrders(customerId)
+                .Join( _db.Products, a => a.ProductId, b => b.Id, (a, b) => b).Distinct()
+                 .ToArray();
+            if (productPurchased == null)
+            {
+                throw new InvalidOperationException("Invalid id customer");
+            }
+            return productPurchased;
+        }
+        public Product[] GetUniqueProductsPurchased(int customerId)
+        {
+            return GetOrders(customerId)
+                .Join(_db.Products, (o) => o.ProductId, (p) => p.Id, (o, p) => p)
+                .Distinct()
+                .ToArray();
+        }
+    }
+}
